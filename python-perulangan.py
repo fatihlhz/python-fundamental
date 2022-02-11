10e28a5f124ae6d148d940ffbf18f204db9025ff
fatihlhz <alfatih.hergun@gmail.com>
2022-02-11 14:27:09 +0700

    commit new sequence

---
+++ python-perulangan.py
@@ -0,0 +1,16 @@
+
+# Perulangan for
+
+jumlah_buku = 10
+
+sudah_dibaca = 0
+
+for sudah_dibaca in range(1, 11):
+    print(f"Buku ke {sudah_dibaca} sudah dibaca")
+
+
+# Perulangan while
+
+while sudah_dibaca < jumlah_buku:
+    sudah_dibaca += 1
+    print(f"Buku ke {sudah_dibaca} sudah dibaca")
